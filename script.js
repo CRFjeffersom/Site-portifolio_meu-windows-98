11c0f525ede1f4e1a85267d8e2bcca15d7a7ef4a
Jeffersom <jeffersombenvindo@gmail.com>
2026-07-09 21:02:10 -0300

    versão-inicial
    versão 0.1

---
+++ script.js
@@ -0,0 +1,35 @@
+
+// relogio  
+
+function atualizarRelogio() {
+    const agora = new Date();
+
+    let horas = agora.getHours();
+    let minutos = agora.getMinutes();
+
+    horas = String(horas).padStart(2, "0");
+    minutos = String(minutos).padStart(2, "0");
+
+    document.getElementById("relogio").textContent = `${horas}:${minutos}`;
+}
+
+atualizarRelogio();
+
+setInterval(atualizarRelogio, 1000);
+
+//Menu iniciar
+
+const botao = document.getElementById("iniciar-botao");
+const menu = document.getElementById("menu-iniciar");
+
+botao.onclick = function () {
+
+    if (menu.style.display === "flex") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "flex";
+    }
+
+}
+
+//
