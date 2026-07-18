f4697d767f21e081d017f4d93709769c46c3c38e
Jeffersom <jeffersombenvindo@gmail.com>
2026-07-18 16:01:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ function criarJanela(titulo){
             <span>${titulo}</span>
 
             <button class="fechar">X</button>
-            <button class="fechar">X</button>
+            
         </div>
 
         <div class="window-content">
